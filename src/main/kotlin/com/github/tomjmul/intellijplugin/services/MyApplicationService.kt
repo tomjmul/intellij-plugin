2b098d30d584dd7a923f546c5955bf8b7a88e056
GitHub Action <action@github.com>
2020-07-16 15:52:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/tomjmul/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tomjmul/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tomjmul.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tomjmul.intellijplugin.MyBundle
 
 class MyApplicationService {
 
